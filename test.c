0d8373f01fa4159f84eff1af4dd118d9a2533ac3
Mehmet Alp ARSLAN <5alparslanmehmet8@gmail.com>
2025-10-05 14:58:07 +0300

    aritmetik-islemler

--- test.c
+++ test.c
@@ -1,10 +1,21 @@
-#include<stdio.h>
+#include<stdio.h> //standart input output kütüphanesi ekleniyor 
 
-int main() {
-    printf("BTK akademi derslerime başlıyorum\n");
+int main() { //programın başlangıç noktası
+    
+    //değişken tanımlama
+    int sayi1, sayi2, toplam, carpim, mod, fark; //tam sayı veri tipi tanımlama
+    float bolum; //ondalıklı sayı veri tipi tanımlama
 
-    return 0;
-}
+    //Kullanıcıdan iki sayı alma 
+    printf("Lütfen iki sayı giriniz: "); //ekrana yazdırma fonksiyonu
+    scanf("%d %d", &sayi1, &sayi2); //kullanıcıdan veri alma fonksiyonu
+    
+    toplam = sayi1 + sayi2; //toplama işlemi
+    carpim = sayi1 * sayi2; //çarpma işlemi
+    fark = sayi1 - sayi2; //çıkarma işlemi
+    bolum = (float)sayi1 / sayi2; //bölme işlemi
+    mod = sayi1 % sayi2; //mod alma işlemi  
+    
 
-
-
+    return 0;       //programın sonlanması
+}               
